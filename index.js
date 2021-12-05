dd63598cd3fea894d30cade84d4d0c06c7b1f196
Naresh1310 <72747482+Naresh1310@users.noreply.github.com>
2021-12-05 22:15:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var menu1=document.getElementById("menu");
+function showmenu(){
+ menu1.style.right="0";
+ menu1.style.visibility="visible";
+}
+function hidemenu(){
+    menu1.style.visibility="hidden";
+}
